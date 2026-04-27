4c3bc930dcc04610316e02d8f66a17100d8c9db7
Davi Baptista <davi@mothergames.com>
2026-04-27 16:36:11 -0300

    Feat: Site changes

--- js/index.js
+++
@@ -1,8 +0,0 @@
-let loading = document.querySelector('.loading');
-
-window.addEventListener('load', (event) => {
-    loading.classList.add("fade-out");
-    setTimeout(() => {
-        loading.classList.add("hidden");
-    }, 1000);
-})
